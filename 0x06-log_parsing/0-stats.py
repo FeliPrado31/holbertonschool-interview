75eda18e33807d88b2db03cecca0538ba32a3d24
Feli <srgatoiscool@gmail.com>
2020-09-02 19:16:01 -0500

    try fix #2

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,44 +5,28 @@ Write a script that reads stdin line by line and computes metrics
 
 import sys
 
-
-total_size = 0
-counter = 0
-status_code = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0,
-
-}
-
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 2:
-            size = line_list[-1]
-            code = line_list[-2]
-            if code in status_code:
-                status_code[code] += 1
+    count = 0
+    size = 0
+    statusErrors = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
 
-            total_size += int(size)
-            counter += 1
-            if counter == 10:
-                print("File size: {:d}".format(total_size))
-                s_code = sorted(status_code.keys())
-                for key in s_code:
-                    if status_code[key] is not 0:
-                        print("{}: {}".format(key, status_code[key]))
-                counter = 0
-except Exception:
+    for line in sys.stdin:
+        count += 1
+        data = line.split()
+        num = data[-2]
+        if num in statusErrors:
+            statusErrors[num] += 1
+        size += int(data[-1])
+        if count % 10 == 0:
+            print("File size: {}".format(size))
+            for i, j in sorted(statusErrors.items()):
+                if j > 0:
+                    print("{}: {}".format(i, j))
+except KeyboardInterrupt:
     pass
 finally:
-    print("File size: {}".format(total_size))
-    s_code = sorted(status_code.keys())
-    for key in s_code:
-        if status_code[key] is not 0:
-            print("{}: {}".format(key, status_code[key]))
+    print("File size: {}".format(size))
+    for i, j in sorted(statusErrors.items()):
+        if j > 0:
+            print("{}: {}".format(i, j))
