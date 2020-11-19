32f1e7dd2dfd93273ae7026b3ddd5db6899e01d8
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-19 20:59:54 +0000

    semistandar

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,26 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-
 const arg = process.argv.slice(2);
+const ENDPOINT = 'https://swapi-api.hbtn.io/api/films';
 
-if (!arg.length) { process.exit(1); }
+if (!arg.length) {
+  process.exit(1);
+}
 
-request.get(`https://swapi-api.hbtn.io/api/films/${arg}/`, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-    return;
-  }
+request.get(`${ENDPOINT}/${arg}/`, async (error, response, body) => {
+  if (error) return console.error(error);
+
   const chars = JSON.parse(body).characters;
   for (const char of chars) {
-    const res = await new Promise((resolve, reject) => {
-      request(char, (error, res, body2) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body2).name);
-        }
-      });
-    });
+    const res = await new Promise((resolve, reject) => request(char, (error, res, body2) => error ? reject(error) : resolve(JSON.parse(body2).name)));
     console.log(res);
   }
 });
